99fd4bf7dcbc9a8d8be4bc97e8c098e421135d8c
leannebb <jleannejia@gmail.com>
2016-05-04 22:44:44 -0700

    newResume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,7 @@
-$( "#main" ).append( "<p>Test</p>" );
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+ var formattedName = HTMLheaderName.replace("%data%", "Lijia Li");
+ 
+ $("#header").append(formattedName);
