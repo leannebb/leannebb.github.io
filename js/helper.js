089362ab49bde19a2d77c478a40c953220d493c5
leannebb <jleannejia@gmail.com>
2016-05-06 11:42:37 -0700

    final resume

--- js/helper.js
+++ js/helper.js
@@ -16,12 +16,12 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">330-389-6124</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">jleannejia@gmail.com</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">leannebb</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">leannebb</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">leannebb</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">Tacoma</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -239,7 +239,7 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,182 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
  var formattedName = HTMLheaderName.replace("%data%", "Lijia Li");
+ var role = "Web Developer";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+$("#topContacts").append(HTMLmobile);
+$("#topContacts").append(HTMLemail);
+$("#topContacts").append(HTMLgithub);
+$("#topContacts").append(HTMLlocation);
  
- $("#header").append(formattedName);
+var bio ={
+	"name" : "Lijia Li",
+	"role" : "Web Developer",
+	"contacts" :{
+		"mobile" : "330-389-6124",
+		"email" : "jleannejia@gmail.com",
+		"github" : "leannebb",
+		"location" : "Tacoma"
+	},
+
+	"welcomeMessage" : "Hello My friends",
+	"skills" : ["Java","HTML","JS","JQuery","CSS","D3.js","ASP.net"],
+	"bioPic" : "img/null"
+}
+
+
+var education ={
+	"schools" : [
+		{
+			"name" : "Kent State University",
+			"city" : "Kent, OH",
+			"degree" : "Master",
+			"major" : ["CS"],
+			"dates" : "2015",
+			"url" : "http://kent.edu"
+		},
+		{
+			"name" : "Chongqing University of Posts and Telecommunications",
+			"city" : "Chongqing, China",
+			"degree" : "BS",
+			"major" : ["CS"],
+			"dates" : "2007",
+			"url" : "http://cqupt.edu.cn"
+		}
+	]
+
+}
+
+var work = {
+	"jobs":[
+		{
+			"employer" :"Chongqing TV Station",
+			"title" : "News Director",
+			"dates" : "August 2007 -- June 2011",
+			"description" : "The News Director (ND) leads and manages the planning, production and presentation of news. The ND supervises the news department staff. The ND reports to (the program director / general manager) and works in cooperation with station leadership and staff to support the station mission."
+		}
+	]
+}
+
+var projects ={
+	"projects" :[
+		{
+			"title" :"Text Filtering Applicaton for College of Business",
+			"dates" : "May 2012 -- August 2013",
+			"description" : "Developed Text filtering application using Perl.Designed an application for visualizing commercial data with D3.js."
+		},
+
+		{
+			"title" :"Website develop and maintain",
+			"dates" : "December 2012 -- January 2014",
+			"description" : "Designed website for Chinese Students and Scholars Association "
+		},
+
+		{
+			"title" :"Real-time Text Visualization Analysis System ",
+			"dates" : "May 2013 -- August 2014",
+			"description" : "Designed website for Chinese Students and Scholars Association "
+		},
+
+		{
+			"title" :"Kent Explore ",
+			"dates" : "Mar 2015 -- May 2015",
+			"description" : "Constructed a map_based website to enable travelers to explore Kent Ohio Provide key information such as map service, points of interests and recommendations based on Google Maps API,HTML,CSS,JavaScript,JQuery,Python."
+		}
+
+	]
+}
+
+
+
+
+
+
+ 
+
+function displayWork(){
+
+   
+    
+
+ 	$("#header").append(HTMLskillsStart);
+
+		for (var i =0 ; i < bio.skills.length; i++) {
+        	 var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+	
+	  	$("#skills").append(formattedSkill);
+    }
+       
+      
+     
+       
+       
+
+	for(job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+		var formattedEmployerTitle = formattedEmployer+formattedTitle;
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+
+
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+         var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+	}
+
+}
+
+displayWork();
+
+
+
+/* for fun */
+function locationizer(work_obj){
+	var locationArray = [];
+
+	for(job in work_obj.jobs){
+		var newLocation = work_obj.jobs[job].location;
+		locationArray.push(newLocation);
+	}
+    return locationArray;
+}
+
+function inName(name){
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1]=name[1].toUpperCase();
+	name[0]=name[0].slice(0,1).toUpperCase()+name[0].slice(1).toLowerCase();
+
+	return name[0] +" " +name[1];
+}
+
+$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
+
+
